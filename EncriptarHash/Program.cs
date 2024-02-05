1d2b817020b4c42afce9a33d0f104727aaf6ab5f
julio.sanchez <julio.sanchez@DEVCUMPDELL>
2024-02-05 09:55:27 -0600

    Agregar archivos de proyecto.

---
+++ EncriptarHash/Program.cs
@@ -0,0 +1,34 @@
+﻿using EncriptarHash;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Ejemplo de uso de BCrypt 
+
+        var usuario = new Usuario
+        {
+            Id = 1,
+            UserName = "admin",
+            Salt = BCrypt.Net.BCrypt.GenerateSalt(),
+            PasswordHash = BCrypt.Net.BCrypt.HashPassword("1234")
+        };
+
+        Console.Write("Ingrese el nombre de usuario: ");
+        string inputUsername = Console.ReadLine();
+        Console.Write("Ingrese la contraseña: ");
+        string inputPassword = Console.ReadLine();
+
+        var usuarioExistente = usuario.UserName == inputUsername;
+
+        if (usuarioExistente != null && BCrypt.Net.BCrypt.Verify(inputPassword, usuario.PasswordHash))
+        {
+            Console.WriteLine("Inicio de sesión exitoso.");
+        }
+        else
+        {
+            Console.WriteLine("Credenciales incorrectas. Inicio de sesión fallido.");
+        }
+    }
+}
---
+++ EncriptarHash/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncriptarHash
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string PasswordHash { get; set; }
+        public string Salt { get; set; }
+    }
+}
